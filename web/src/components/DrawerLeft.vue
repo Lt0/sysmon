6411591ac8b2f756276f43a18f0f65871871f6ac
lightime <lightimehpq@gmail.com>
2018-05-09 00:40:03 +0800

    搭建 web 界面的基本框架

---
+++ web/src/components/DrawerLeft.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-navigation-drawer v-model="drawer" clipped fixed app >
+        <v-list>
+            <v-list-tile @click="">
+                <v-list-tile-action><v-icon>show_chart</v-icon></v-list-tile-action>
+                <v-list-tile-content><v-list-tile-title> Resources </v-list-tile-title></v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile @click="">
+                <v-list-tile-action><v-icon>list</v-icon></v-list-tile-action>
+                <v-list-tile-content><v-list-tile-title>Processes</v-list-tile-title></v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile @click="">
+                <v-list-tile-action><v-icon>storage</v-icon></v-list-tile-action>
+                <v-list-tile-content><v-list-tile-title>File Systems</v-list-tile-title></v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile @click="">
+                <v-list-tile-action><v-icon>settings</v-icon></v-list-tile-action>
+                <v-list-tile-content><v-list-tile-title>Settings</v-list-tile-title></v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    name: 'DrawerLeft',
+    props: ['drawer'],
+    data () {
+
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- web/src/components/Layout.vue
+++ web/src/components/Layout.vue
@@ -1,46 +1,59 @@
 <template>
-    <div>
-        <v-navigation-drawer app>nav panel</v-navigation-drawer>
-        <v-toolbar app>
-            <v-toolbar-side-icon></v-toolbar-side-icon>
-            <v-toolbar-title class="white--text">Title</v-toolbar-title>
+    <v-app id="inspire">
+        <drawer-left :drawer='drawer' />
+        <v-toolbar app fixed clipped-left color="teal lighten-1" dark>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>System Monitor</v-toolbar-title>
+
             <v-spacer></v-spacer>
-            <v-btn icon>
-            <v-icon>search</v-icon>
-            </v-btn>
-            <v-btn icon>
-            <v-icon>apps</v-icon>
-            </v-btn>
-            <v-btn icon>
-            <v-icon>refresh</v-icon>
-            </v-btn>
-            <v-btn icon>
-            <v-icon>more_vert</v-icon>
-            </v-btn>
+
+            <v-btn icon><v-icon>more_vert</v-icon></v-btn>
         </v-toolbar>
-        
+
         <v-content>
-            <v-container fluid>
-                <div>
-                    {{msg}}
-                </div>
+            <v-container fluid fill-height>
+                <v-layout justify-center align-center>
+                <v-flex shrink>
+                    <v-tooltip right>
+                    <v-btn
+                        slot="activator"
+                        :href="source"
+                        icon
+                        large
+                        target="_blank"
+                    >
+                        <v-icon large>code</v-icon>
+                    </v-btn>
+                    <span>Source</span>
+                    </v-tooltip>
+                    <v-tooltip right>
+                    <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/qxQWda" target="_blank">
+                        <v-icon large>mdi-codepen</v-icon>
+                    </v-btn>
+                    <span>Codepen</span>
+                    </v-tooltip>
+                </v-flex>
+                </v-layout>
             </v-container>
         </v-content>
-        <v-footer app>lightimehpq@gmail.com</v-footer>
-  </div>
+
+        <v-footer app fixed>
+            <span>&copy; lightimehpq@gmail.com</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-import Toolbar from '@/components/Toolbar'
+import DrawerLeft from '@/components/DrawerLeft'
 
 export default {
   name: 'Layout',
   components: {
-      Toolbar
+      DrawerLeft
   },
   data () {
     return {
-      msg: 'layout all things here'
+      drawer: true,
     }
   }
 }
