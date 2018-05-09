2c66b22f7f1959be3bb471073691739f75097480
canvor <lightimehpq@gmail.com>
2018-05-09 10:10:57 +0800

    前端路由重定向 / 到 /resources

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -15,6 +15,7 @@ export default new Router({
       path: '/',
       name: 'Layout',
       component: Layout,
+      redirect: Resources,
       children: [
         {
           path: 'resources',
