313123896874fa95b1d51867fec02edc9f63b182
lightime <lightimehpq@gmail.com>
2018-05-29 00:42:14 +0800

    实现 process 的显示项可选功能，并根据屏幕宽度动态决定显示项目数量，目前可以兼容各种宽度的屏幕，包括移动端

--- web/src/components/content/processes/processList.vue
+++ web/src/components/content/processes/processList.vue
@@ -1,7 +1,11 @@
 <template>
-    <v-card id="process">
+    <div class="process-container">
+    <v-card id="process" v-resize="onResize">
         <v-card-title>
+            <selection style='padding-top: 12px;' :items="items" v-model='selectedItems' defaultItemNum=3 />
+
             <v-spacer></v-spacer>
+            
             <v-text-field
                 v-model="search"
                 append-icon="search"
@@ -34,45 +38,45 @@
                 </v-tooltip>
             </template>
             <template slot="items" slot-scope="props">
-                <td class="text-xs-left">{{ props.item.User }}</td>
-                <td class="text-xs-left">{{ props.item.Cmd }}</td>
-                <td class="text-xs-left">{{ props.item.CPU}}</td>
-                <td class="text-xs-left">{{ props.item.CPUTime }}</td>
-                <td class="text-xs-left">{{ props.item.MEM }}</td>
-                <td class="text-xs-left">
+                <td class="text-xs-left" v-if="displayUser">{{ props.item.User }}</td>
+                <td class="text-xs-left" v-if="displayCmd">{{ props.item.Cmd }}</td>
+                <td class="text-xs-left" v-if="displayCPU">{{ props.item.CPU}}</td>
+                <td class="text-xs-left" v-if="displayCPUTime">{{ props.item.CPUTime }}</td>
+                <td class="text-xs-left" v-if="displayMEM">{{ props.item.MEM }}</td>
+                <td class="text-xs-left" v-if="displaySZ">
                     <v-tooltip bottom>
                         <span slot="activator">{{cm.fmtSize.fmtKBSize(props.item.SZ, 2)}}</span>
                         <span>{{props.item.SZ}}KB</span>
                     </v-tooltip>
                 </td>
-                <td class="text-xs-left">
+                <td class="text-xs-left" v-if="displayRSS">
                     <v-tooltip bottom>
                         <span slot="activator">{{cm.fmtSize.fmtKBSize(props.item.RSS, 2)}}</span>
                         <span>{{props.item.RSS}}KB</span>
                     </v-tooltip>
                 </td>
-                <td class="text-xs-left">
+                <td class="text-xs-left" v-if="displayDRS">
                     <v-tooltip bottom>
                         <span slot="activator">{{cm.fmtSize.fmtKBSize(props.item.DRS, 2)}}</span>
                         <span>{{props.item.DRS}}KB</span>
                     </v-tooltip>
                 </td>
-                <td class="text-xs-left">
+                <td class="text-xs-left" v-if="displayTRS">
                     <v-tooltip bottom>
                         <span slot="activator">{{cm.fmtSize.fmtKBSize(props.item.TRS, 2)}}</span>
                         <span>{{props.item.TRS}}KB</span>
                     </v-tooltip>
                 </td>
-                <td class="text-xs-left">
+                <td class="text-xs-left" v-if="displayVSZ">
                     <v-tooltip bottom>
                         <span slot="activator">{{cm.fmtSize.fmtKBSize(props.item.VSZ, 2)}}</span>
                         <span>{{props.item.VSZ}}KB</span>
                     </v-tooltip>
                 </td>
-                <td class="text-xs-left">{{ props.item.Pid }}</td>
-                <td class="text-xs-left">{{ props.item.Nlwp }}</td>
-                <td class="text-xs-left">{{ props.item.State }}</td>
-                <td class="text-xs-left">{{ props.item.Nice }}</td>
+                <td class="text-xs-left" v-if="displayPid">{{ props.item.Pid }}</td>
+                <td class="text-xs-left" v-if="displayNlwp">{{ props.item.Nlwp }}</td>
+                <td class="text-xs-left" v-if="displayState">{{ props.item.State }}</td>
+                <td class="text-xs-left" v-if="displayNice">{{ props.item.Nice }}</td>
             </template>
 
             <!-- 没有数据时显示的内容 -->
@@ -88,36 +92,149 @@
             </v-alert>
         </v-data-table>
     </v-card>
+    </div>
 </template>
 
 <script>
+import selection from '@/components/common/selection'
 import cm from '../../../js/common'
 
 export default {
     name: 'processList',
+    components: {
+        selection, 
+    },
     props: ['info'],
     data() {
         return {
             cm: cm,
+            windowSize: {
+                x: 0,
+                y: 0
+            },
             search: '',
-            headers: [
-                { text: 'User', value: 'User' },
-                { text: 'Name', value: 'Cmd' },
-                { text: 'CPU', value: 'CPU' },
-                { text: 'CPUTime', value: 'CPUTime' },
-                { text: 'MEM(%)', value: 'MEM' },
-                { text: 'SZ', value: 'SZ' },
-                { text: 'RSS', value: 'RSS' },
-                { text: 'DRS', value: 'DRS' },
-                { text: 'TRS', value: 'TRS' },
-                { text: 'VSZ', value: 'VSZ' },
-                { text: 'Pid', value: 'Pid' },
-                { text: 'Nlwp', value: 'Nlwp' },
-                { text: 'State', value: 'State' },
-                { text: 'Nice', value: 'Nice' },
-            ],
+            items: ['Cmd', 'CPU', 'MEM', 'User', 'CPUTime', 'SZ', 'RSS', 'DRS', 'TRS', 'VSZ', 'Pid', 'Nlwp', 'State', 'Nice'],
+            selectedItems: [],
+            selectTypes: null,
+            displayUser: false,
+            displayCmd: true,
+            displayCPU: true,
+            displayCPUTime: false,
+            displayMEM: true,
+            displaySZ: false,
+            displayRSS: false,
+            displayDRS: false,
+            displayTRS: false,
+            displayVSZ: false,
+            displayPid: false,
+            displayNlwp: false,
+            displayState: false,
+            displayNice: false,
+            headers: [],
         }
-    }
+    },
+    watch: {
+        selectedItems: function(){
+            this.updateHeaders();
+            this.updateDisplay();
+        },
+        // 根据屏幕大小变化，调整显示的项目，每 135px 宽显示一个项目
+        windowSize: function(){
+            let num = Math.floor(this.windowSize.x/135);
+            this.selectedItems = [];
+            for (let i = 0; i < num; i++){
+                if (this.items[i]){
+                    this.selectedItems.push(this.items[i]);
+                } else {
+                    break;
+                }
+                
+            }
+            // console.log("this.selectedItems: " + this.selectedItems);
+            this.updateHeaders();
+            this.updateDisplay();
+        },
+    },
+    methods: {
+        updateHeaders(){
+            // console.log("updateHeaders: " + this.selectedItems);
+            this.headers = [];
+            for (let item in this.selectedItems){
+                let t = this.selectedItems[item];
+                if (this.selectedItems[item] == 'CPU' || this.selectedItems[item] == 'MEM'){
+                    t += "(%)";
+                }
+                let hdr = {text: t, value: this.selectedItems[item]};
+                this.headers.push(hdr);
+            }
+        },
+        updateDisplay(){
+            this.displayUser = false;
+            this.displayCmd = false;
+            this.displayCPU = false;
+            this.displayCPUTime = false;
+            this.displayMEM = false;
+            this.displaySZ = false;
+            this.displayRSS = false;
+            this.displayDRS = false;
+            this.displayTRS = false;
+            this.displayVSZ = false;
+            this.displayPid = false;
+            this.displayNlwp = false;
+            this.displayState = false;
+            this.displayNice = false;
+
+            for (let i in this.selectedItems){
+                switch(this.selectedItems[i]) {
+                    case 'User':
+                        this.displayUser = true;
+                        break;
+                    case 'Cmd':
+                        this.displayCmd = true;
+                        break;
+                    case 'CPU':
+                        this.displayCPU = true;
+                        break;
+                    case 'CPUTime':
+                        this.displayCPUTime = true;
+                        break;
+                    case 'MEM':
+                        this.displayMEM = true;
+                        break;
+                    case 'SZ':
+                        this.displaySZ = true;
+                        break;
+                    case 'RSS':
+                        this.displayRSS = true;
+                        break;
+                    case 'DRS':
+                        this.displayDRS = true;
+                        break;
+                    case 'TRS':
+                        this.displayTRS = true;
+                        break;
+                    case 'VSZ':
+                        this.displayVSZ = true;
+                        break;
+                    case 'Pid':
+                        this.displayPid = true;
+                        break;
+                    case 'Nlwp':
+                        this.displayNlwp = true;
+                        break;
+                    case 'State':
+                        this.displayState = true;
+                        break;
+                    case 'Nice':
+                        this.displayNice = true;
+                        break;
+                }
+            }
+        },
+        onResize(){
+            this.windowSize = { x: window.innerWidth, y: window.innerHeight }
+        },
+    },
 }
 </script>
 
