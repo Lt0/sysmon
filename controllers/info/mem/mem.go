b4372c5499384eaf7ba4ede608137876f2cf9e0d
canvor <lightimehpq@gmail.com>
2018-07-04 17:15:30 +0800

    info/mem 使用 proc util 来获取 /proc/meminfo 的信息

--- controllers/info/mem/mem.go
+++ controllers/info/mem/mem.go
@@ -2,59 +2,60 @@ package mem
 
 import (
 	"fmt"
-	"strings"
 	"os/exec"
 	"strconv"
+
+	"github.com/Lt0/sysmon/utils/proc"
 )
 
 // unit of mem info is KB
 type MemInfo struct {
-	MemTotal int
-	MemFree int
-	MemAvailable int
-	Buffers int
-	Cached int
-	SwapCached int
-	Active int
-	Inactive int
-	ActiveAnon int
-	InactiveAnon int
-	ActiveFile int
-	InactiveFile int
-	Unevictable int
-	Mlocked int
-	SwapTotal int
-	SwapFree int
-	Dirty int
-	Writeback int
-	AnonPages int
-	Mapped int
-	Shmem int
-	Slab int
-	SReclaimable int
-	SUnreclaim int
-	KernelStack int
-	PageTables int
-	NfsUnstable int
-	Bounce int
-	WritebackTmp int
-	CommitLimit int
-	CommittedAS int
-	VmallocTotal int
-	VmallocUsed int
-	VmallocChunk int
-	HardwareCorrupted int
-	AnonHugePages int
-	CmaTotal int
-	CmaFree int
-	HugePagesTotal int
-	HugePagesFree int
-	HugePagesRsvd int
-	HugePagesSurp int
-	Hugepagesize int
-	DirectMap4k int
-	DirectMap2M int
-	DirectMap1G int
+	MemTotal uint64
+	MemFree uint64
+	MemAvailable uint64
+	Buffers uint64
+	Cached uint64
+	SwapCached uint64
+	Active uint64
+	Inactive uint64
+	ActiveAnon uint64
+	InactiveAnon uint64
+	ActiveFile uint64
+	InactiveFile uint64
+	Unevictable uint64
+	Mlocked uint64
+	SwapTotal uint64
+	SwapFree uint64
+	Dirty uint64
+	Writeback uint64
+	AnonPages uint64
+	Mapped uint64
+	Shmem uint64
+	Slab uint64
+	SReclaimable uint64
+	SUnreclaim uint64
+	KernelStack uint64
+	PageTables uint64
+	NfsUnstable uint64
+	Bounce uint64
+	WritebackTmp uint64
+	CommitLimit uint64
+	CommittedAS uint64
+	VmallocTotal uint64
+	VmallocUsed uint64
+	VmallocChunk uint64
+	HardwareCorrupted uint64
+	AnonHugePages uint64
+	CmaTotal uint64
+	CmaFree uint64
+	HugePagesTotal uint64
+	HugePagesFree uint64
+	HugePagesRsvd uint64
+	HugePagesSurp uint64
+	Hugepagesize uint64
+	DirectMap4k uint64
+	DirectMap2M uint64
+	DirectMap1G uint64
 
 	UsedMem int	// 通过 free 获取，以 KB 为单位
 }
@@ -68,111 +69,53 @@ func All() MemInfo {
 }
 
 func GetMemInfo(mi *MemInfo) {
-	out, err := exec.Command("sh", "-c", `cat /proc/meminfo | sed 's/:\s*/:/g' | sed 's/ kB$//g'`).Output()
-	if err != nil {
-		fmt.Println("GetMemInfo", err)
-		return
-	}
-
-	for _, v := range(strings.Split(string(out), "\n")) {
-		if v == "" {continue}
-		attrStr := strings.Split(v, ":")
-		switch attrStr[0] {
-		case "MemTotal":
-			mi.MemTotal, _ = strconv.Atoi(attrStr[1]) 
-		case "MemFree":
-			mi.MemFree, _ = strconv.Atoi(attrStr[1]) 
-		case "MemAvailable":
-			mi.MemAvailable, _ = strconv.Atoi(attrStr[1]) 
-		case "Buffers":
-			mi.Buffers, _ = strconv.Atoi(attrStr[1]) 
-		case "Cached":
-			mi.Cached, _ = strconv.Atoi(attrStr[1]) 
-		case "SwapCached":
-			mi.SwapCached, _ = strconv.Atoi(attrStr[1]) 
-		case "Active":
-			mi.Active, _ = strconv.Atoi(attrStr[1]) 
-		case "Inactive":
-			mi.Inactive, _ = strconv.Atoi(attrStr[1]) 
-		case "Active(anon)":
-			mi.ActiveAnon, _ = strconv.Atoi(attrStr[1]) 
-		case "Inactive(anon)":
-			mi.InactiveAnon, _ = strconv.Atoi(attrStr[1]) 
-		case "Active(file)":
-			mi.ActiveFile, _ = strconv.Atoi(attrStr[1]) 
-		case "Inactive(file)":
-			mi.InactiveFile, _ = strconv.Atoi(attrStr[1]) 
-		case "Unevictable":
-			mi.Unevictable, _ = strconv.Atoi(attrStr[1]) 
-		case "Mlocked":
-			mi.Mlocked, _ = strconv.Atoi(attrStr[1]) 
-		case "SwapTotal":
-			mi.SwapTotal, _ = strconv.Atoi(attrStr[1]) 
-		case "SwapFree":
-			mi.SwapFree, _ = strconv.Atoi(attrStr[1]) 
-		case "Dirty":
-			mi.Dirty, _ = strconv.Atoi(attrStr[1]) 
-		case "Writeback":
-			mi.Writeback, _ = strconv.Atoi(attrStr[1]) 
-		case "AnonPages":
-			mi.AnonPages, _ = strconv.Atoi(attrStr[1]) 
-		case "Mapped":
-			mi.Mapped, _ = strconv.Atoi(attrStr[1]) 
-		case "Shmem":
-			mi.Shmem, _ = strconv.Atoi(attrStr[1]) 
-		case "Slab":
-			mi.Slab, _ = strconv.Atoi(attrStr[1]) 
-		case "SReclaimable":
-			mi.SReclaimable, _ = strconv.Atoi(attrStr[1]) 
-		case "SUnreclaim":
-			mi.SUnreclaim, _ = strconv.Atoi(attrStr[1]) 
-		case "KernelStack":
-			mi.KernelStack, _ = strconv.Atoi(attrStr[1]) 
-		case "PageTables":
-			mi.PageTables, _ = strconv.Atoi(attrStr[1]) 
-		case "NFS_Unstable":
-			mi.NfsUnstable, _ = strconv.Atoi(attrStr[1]) 
-		case "Bounce":
-			mi.Bounce, _ = strconv.Atoi(attrStr[1]) 
-		case "WritebackTmp":
-			mi.WritebackTmp, _ = strconv.Atoi(attrStr[1]) 
-		case "CommitLimit":
-			mi.CommitLimit, _ = strconv.Atoi(attrStr[1]) 
-		case "Committed_AS":
-			mi.CommittedAS, _ = strconv.Atoi(attrStr[1]) 
-		case "VmallocTotal":
-			mi.VmallocTotal, _ = strconv.Atoi(attrStr[1]) 
-		case "VmallocUsed":
-			mi.VmallocUsed, _ = strconv.Atoi(attrStr[1]) 
-		case "VmallocChunk":
-			mi.VmallocChunk, _ = strconv.Atoi(attrStr[1]) 
-		case "HardwareCorrupted":
-			mi.HardwareCorrupted, _ = strconv.Atoi(attrStr[1]) 
-		case "AnonHugePages":
-			mi.AnonHugePages, _ = strconv.Atoi(attrStr[1]) 
-		case "CmaTotal":
-			mi.CmaTotal, _ = strconv.Atoi(attrStr[1]) 
-		case "CmaFree":
-			mi.CmaFree, _ = strconv.Atoi(attrStr[1]) 
-		case "HugePages_Total":
-			mi.HugePagesTotal, _ = strconv.Atoi(attrStr[1]) 
-		case "HugePages_Free":
-			mi.HugePagesFree, _ = strconv.Atoi(attrStr[1]) 
-		case "HugePages_Rsvd":
-			mi.HugePagesRsvd, _ = strconv.Atoi(attrStr[1]) 
-		case "HugePages_Surp":
-			mi.HugePagesSurp, _ = strconv.Atoi(attrStr[1]) 
-		case "Hugepagesize":
-			mi.Hugepagesize, _ = strconv.Atoi(attrStr[1]) 
-		case "DirectMap4k":
-			mi.DirectMap4k, _ = strconv.Atoi(attrStr[1]) 
-		case "DirectMap2M":
-			mi.DirectMap2M, _ = strconv.Atoi(attrStr[1]) 
-		default:
-		}
-	}
-
-	
+	m, _ := proc.GetMeminfo()
+	mi.MemTotal = m.MemTotal
+	mi.MemFree = m.MemFree
+	mi.MemAvailable = m.MemAvailable
+	mi.Buffers = m.Buffers
+	mi.Cached = m.Cached
+	mi.SwapCached = m.SwapCached
+	mi.Active = m.Active
+	mi.Inactive = m.Inactive
+	mi.ActiveAnon = m.ActiveAnon
+	mi.InactiveAnon = m.InactiveAnon
+	mi.ActiveFile = m.ActiveFile
+	mi.InactiveFile = m.InactiveFile
+	mi.Unevictable = m.Unevictable
+	mi.Mlocked = m.Mlocked
+	mi.SwapTotal = m.SwapTotal
+	mi.SwapFree = m.SwapFree
+	mi.Dirty = m.Dirty
+	mi.Writeback = m.Writeback
+	mi.AnonPages = m.AnonPages
+	mi.Mapped = m.Mapped
+	mi.Shmem = m.Shmem
+	mi.Slab = m.Slab
+	mi.SReclaimable = m.SReclaimable
+	mi.SUnreclaim = m.SUnreclaim
+	mi.KernelStack = m.KernelStack
+	mi.PageTables = m.PageTables
+	mi.NfsUnstable = m.NfsUnstable
+	mi.Bounce = m.Bounce
+	mi.WritebackTmp = m.WritebackTmp
+	mi.CommitLimit = m.CommitLimit
+	mi.CommittedAS = m.CommittedAS
+	mi.VmallocTotal = m.VmallocTotal
+	mi.VmallocUsed = m.VmallocUsed
+	mi.VmallocChunk = m.VmallocChunk
+	mi.HardwareCorrupted = m.HardwareCorrupted
+	mi.AnonHugePages = m.AnonHugePages
+	mi.CmaTotal = m.CmaTotal
+	mi.CmaFree = m.CmaFree
+	mi.HugePagesTotal = m.HugePagesTotal
+	mi.HugePagesFree = m.HugePagesFree
+	mi.HugePagesRsvd = m.HugePagesRsvd
+	mi.HugePagesSurp = m.HugePagesSurp
+	mi.Hugepagesize = m.Hugepagesize
+	mi.DirectMap4k = m.DirectMap4k
+	mi.DirectMap2M = m.DirectMap2M
+	mi.DirectMap1G = m.DirectMap1G	
 }
 
 func GetUsedMem(mi *MemInfo) {
