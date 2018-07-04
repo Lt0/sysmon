f11a1613ce8b67fb4a5bdd9a6827a6c3c9d1662e
lightime <lightimehpq@gmail.com>
2018-07-04 23:29:08 +0800

    实现 proc/pid/cmdline 和 proc/pid/task 工具

---
+++ utils/proc/pid/cmdline.go
@@ -0,0 +1,36 @@
+package pid
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"path/filepath"
+)
+
+// comment for /proc/pid/cmdline
+// This read-only file holds the complete command line for the process, unless the process is a zombie.  In the latter
+// case, there is nothing in this file: that is, a read on this file will return 0 characters.  The command-line argu‐
+// ments  appear  in  this file as a set of strings separated by null bytes ('\0'), with a further null byte after the
+// last string.
+
+type CmdlineInfo struct {
+	Cmdline string
+}
+
+func Cmdline(pid string) (CmdlineInfo, error) {
+	var ci CmdlineInfo
+
+	f, err := os.Open(filepath.Join("/proc", pid, "cmdline"))
+	if err != nil {
+		return ci, fmt.Errorf("Cmdline: open /proc/%v/cmdline failed: %v\n", pid, err)
+	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	cmdline, _ := r.ReadBytes('\n')
+	if len(cmdline) > 1 {
+		ci.Cmdline = string(cmdline[:len(cmdline)-1])
+	}
+	
+	return ci, nil
+}
---
+++ utils/proc/pid/task.go
@@ -0,0 +1,27 @@
+package pid
+
+import (
+	"os"
+	"fmt"
+	"path/filepath"
+)
+
+type TaskInfo []string
+
+func Task(pid string) TaskInfo {
+	t, err := os.Open(filepath.Join("/proc", pid, "task"))
+	if err != nil {
+		fmt.Println("can not open /proc")
+		return nil
+	}
+
+	defer t.Close()
+
+	var tasks TaskInfo
+	tasks, err = t.Readdirnames(0)
+	if err != nil {
+		fmt.Println("Readdirnames /proc failed")
+		return nil
+	}
+	return tasks
+}
