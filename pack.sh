db9bbff5e11481de793103cdc9812d6983df1807
hpq <hepeiqin@casachina.com.cn>
2018-07-25 11:38:56 +0800

    完善打包功能

--- pack.sh
+++ pack.sh
@@ -11,4 +11,13 @@ export CC="musl-gcc"
 go build -o sysmon -ldflags '-linkmode "external" -extldflags "-static"'
 
 echo "pack..."
-tar zcvf sysmon.tar.gz sysmon views conf web/dist swagger
+
+DST=$(mktemp -d)
+mkdir $DST/sysmon
+DST=$DST/sysmon
+cp -rvf sysmon views conf web/dist $DST
+mkdir $DST/web
+mv $DST/dist $DST/web/
+sed -i 's/runmode = dev/runmode = prod/g' $DST/conf/app.conf
+cd $DST/../ && tar zcvf $WS/sysmon.tar.gz *
+cd $WS
