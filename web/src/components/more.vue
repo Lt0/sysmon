77e557709e94ee7410ca1120891a57d6b4f459e4
hpq <hepeiqin@casachina.com.cn>
2018-07-26 12:32:14 +0800

    调整 more 点击的弹窗关闭窗口按钮

--- web/src/components/more.vue
+++ web/src/components/more.vue
@@ -30,7 +30,7 @@
                 <v-divider></v-divider>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="primary" flat @click="aboutDialog = false" > OK </v-btn>
+                    <v-btn color="primary" flat @click="aboutDialog = false" > Close </v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
@@ -42,7 +42,8 @@
                     All custon setting was cleared. Refresh the page to take effect.
                 </v-card-text>
                 <v-card-actions>
-                    
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" flat @click="clearSettingDialog = false" > OK </v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
