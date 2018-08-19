519f49552e4cb8deff42e0d0c3529bf0a87de477
lightime <lightimehpq@gmail.com>
2018-08-19 23:38:12 +0800

    docker 版本的 process 和 resources 支持显示物理机的信息，process 完全正常，resources 的部分功能和物理机的显示结果还不一致

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -3,3 +3,13 @@ set -e
 
 ./pack.sh
 docker build -t sysmon .
+
+if [[ $? -eq 0 ]]; then
+	echo "=================== success ==================="
+	echo "Start dockerized sysmon:"
+	echo "docker run --name sysmon --restart=always -d -p 4096:2048 -v /proc:/hproc --privileged -it sysmon"
+	echo "Visit web UI from localhost:4096 in browser"
+else 
+	echo "=================== failed ==================="
+	echo build image failed...
+fi
