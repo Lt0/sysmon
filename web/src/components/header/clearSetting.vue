af253f1a3991b97b437f04a59f7f06bd70b4ab13
lightime <lightimehpq@gmail.com>
2018-08-31 00:29:01 +0800

    完善清空所有设置的确认功能

---
+++ web/src/components/header/clearSetting.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-card>
+            <v-card-title class="headline " primary-title > Clear All Setting? </v-card-title>
+            <v-card-text>
+                All setting will be cleared. Including added servers, customization of resources page and more...
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn style="color:#26a69a" flat @click='$emit("clearSettingCancel")' > Cancel </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn style="color:grey" flat @click="clearSetting()" > Clear </v-btn>
+                <v-spacer></v-spacer>
+            </v-card-actions>
+       </v-card>
+</template>
+
+<script>
+	export default {
+		name: 'clearSetting',
+		methods: {
+			clearSetting(){
+            	localStorage.clear();
+            	this.$router.push("/init");
+			},
+		},
+	}
+</script>
+
+<style>
+</style>
--- web/src/components/more.vue
+++ web/src/components/more.vue
@@ -1,48 +1,39 @@
 <template>
     <div>
         <v-menu bottom left offset-y min-width="180px">
+        	<!--more button-->
             <v-btn slot="activator" icon><icon>moreif</icon></v-btn>
 
+			<!--option list-->
             <v-list>
                 <v-tooltip bottom>
                     <span slot="activator">
-                        <v-list-tile @click="clearSetting">
-                            <v-list-tile-title>Clear Setting</v-list-tile-title>
-                        </v-list-tile>
+                        <v-list-tile @click="clearSettingDialog = true"><v-list-tile-title><icon>custom-clear</icon>&ensp;Clear Setting</v-list-tile-title></v-list-tile>
                     </span>
                     <span>clear all custom setting</span>
                 </v-tooltip>
                 
-                <v-list-tile @click='aboutDialog = true'>
-                    <v-list-tile-title>About</v-list-tile-title>
-                </v-list-tile>
+                <v-list-tile @click='aboutDialog = true'><v-list-tile-title><icon>about-o</icon>&ensp;About</v-list-tile-title></v-list-tile>
             </v-list>
         </v-menu>
 
+		<!--dialogs--> 
         <about v-model="aboutDialog" />
-
-        <v-dialog v-model="clearSettingDialog" max-width="500px">
-            <v-card>
-                <v-card-title class="headline " primary-title > Success </v-card-title>
-                <v-card-text>
-                    All custon setting was cleared. Refresh the page to take effect.
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="primary" flat @click="clearSettingDialog = false" > OK </v-btn>
-                </v-card-actions>
-            </v-card>
+        <v-dialog v-model="clearSettingDialog" max-width="400px">
+        	<clear-setting @clearSettingCancel="clearSettingDialog = false" />
         </v-dialog>
     </div>
 </template>
 
 <script>
 import about from '@/components/common/about'
+import clearSetting from '@/components/header/clearSetting'
 
 export default {
     name: 'more',
     components: {
         about,
+        clearSetting,
     },
     data() {
         return {
@@ -50,12 +41,5 @@ export default {
             clearSettingDialog: false,
         }
     },
-    methods: {
-        clearSetting() {
-            console.log("clear Setting");
-            localStorage.clear();
-            this.clearSettingDialog = true;
-        },
-    }, 
 }
 </script>
